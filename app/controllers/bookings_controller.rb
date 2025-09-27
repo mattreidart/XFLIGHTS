83d0bc2ae25154b6e5e80be6b59eb23a04b39d67
Nathan Garcia <chilletunes@gmail.com>
2025-09-27 16:37:02 +1000

    Installed Devise for user login

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,10 @@ class BookingsController < ApplicationController
   end
 
   def create
+    # redirect_to booking_path(@booking), notice: "Booking successfully created"
   end
 
   def index
+    # flash[:notice] = "This is a random message"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'users/show'
   get 'bookings/new'
   get 'bookings/create'

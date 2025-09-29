5eff99ba84853ec7d211dfe741f1784c7a0bf59f
mattreid <mattreidart@gmail.com>
2025-09-29 20:29:55 +1000

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
   def show
+    @user = current_user
+    @bookings = @user.bookings.includes(:flight)
   end
 end

5eebbeb80221a7eef16611c8a1c797b47b386a65
mattreid <mattreidart@gmail.com>
2025-09-29 20:20:51 +1000

    the actual bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,15 +3,46 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.includes(:flight)
     # flash[:notice] = "This is a random message"
   end
 
   def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = current_user.bookings.new(flight: @flight)
   end
 
+  def show; end
+
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = current_user.bookings.new(flight: @flight)
+
   def create
+    @flight = Flight.find(params[:flight_id])
+    @booking = current_user.bookings.new(booking_params.merge(flight: @flight, status: 'confirmed'))
+
+    if booking.save
+      redirect_to booking_path, notice: "Booking successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
     # redirect_to booking_path(@booking), notice: "Booking successfully created"
   end
 
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path, notice: "Booking successfully cancelled"
+  end
+
+  private
+
+  def set_booking
+    @booking = current_user.bookings.find(params[:id])
+end
+
+  def booking_params
+    params.require(:booking).permit(:passengers)
+    # or possibly passenger.name and passenger.email etc
+  end
 end

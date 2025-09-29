42166a98d68473c151de37329948d471dabc3b04
mattreid <mattreidart@gmail.com>
2025-09-29 20:05:25 +1000

    bookings controller structure

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,12 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @bookings = current_user.bookings
+    # flash[:notice] = "This is a random message"
+  end
+
   def new
   end
 
@@ -6,7 +14,4 @@ class BookingsController < ApplicationController
     # redirect_to booking_path(@booking), notice: "Booking successfully created"
   end
 
-  def index
-    # flash[:notice] = "This is a random message"
-  end
 end

67d06329305efbe2f362fc9259f82ff2daa1cf1e
Nathan Garcia <chilletunes@gmail.com>
2025-10-04 14:25:25 +1000

    Updated route and search function

--- db/migrate/20251002010110_remove_date_from_flights.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateFromFlights < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :flights, :date, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_02_075928) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_04_024531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +37,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_02_075928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "airline_id", null: false
-    t.integer "price"
     t.datetime "departure"
     t.datetime "arrival"
+    t.integer "price"
     t.index ["airline_id"], name: "index_flights_on_airline_id"
     t.index ["departure"], name: "index_flights_on_departure"
     t.index ["destination"], name: "index_flights_on_destination"

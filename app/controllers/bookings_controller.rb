502739683d63af4b9302999209953f7951c31a1a
Nathan Garcia <chilletunes@gmail.com>
2025-10-11 15:43:54 +1100

    JS integration and styling fixes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,7 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = current_user.bookings.new(flight: @flight)
+    @user = current_user
   end
 
   def show
